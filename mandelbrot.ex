0ba6014dba50144908d87c04d1f407ebe13570d7
feriri <88581641+feriri@users.noreply.github.com>
2022-03-09 14:55:34 +0100

    Update mandelbrot.ex

--- mandelbrot.ex
+++ mandelbrot.ex
@@ -25,7 +25,9 @@ defmodule Mandel do
     """
     def mandelbrot(width, height, x, y, k, depth) do
         trans = fn(w, h) ->
-            Cmplx.new(x + k * (w - 1), y - k * (h - 1))
+            re = x + k * (w - 1)
+            im = y - k * (h - 1)
+            Cmplx.new(re, im)
         end
         rows(width, height, trans, depth, [])
     end
